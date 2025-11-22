57aa8f8594d24ae09be7a988db95b6c5c9ba5ace
stiborlukas <stiborlukas@gmail.com>
2025-11-22 23:19:38 +0100

    necessary comment

--- testing.sh
+++ testing.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-for n in 1 2 3 4 5; do
-  for k in 1 2 3 4 5 6 7 8; do
+
+# YOU HAVE TO CREATE FOLDER "results" BEFORE RUNNING THIS SCRIPT
+for n in 6 7; do
+  for k in 4 5; do
     if [ $k -le $n ]; then
       echo "Testing ${n}x${n} with $k queens..."
-      python queens.py -n $n -k $k -o "results/n${n}_k${k}.cnf" --quiet
+      python queens.py -n $n -k $k -o "results/n${n}_k${k}.cnf"
     fi
   done
 done
